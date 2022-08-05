7c3627bc0e8a3ff93027200336076af92fbf7da7
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-08-06 00:42:46 +0300

    Добавьте файлы проекта.

---
+++ Task1/Angle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value > 0 || value < 60)
+                {
+                    min = value;
+                }
+                else
+                {
+                    min = -1;
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return Sec;
+            }
+            set
+            {
+                if (value > 0 || value < 60)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    sec = -1;
+                }
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadians()
+        {
+            if (min == -1 || sec == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                return (gradus + (min + (double)sec / 60) / 60) * Math.PI / 180;
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool beGradus;
+            bool beMin;
+            bool beSec;
+            int gradus;
+            int min;
+            int sec;
+            Console.WriteLine("Введите целочисленное значение угла в градусах, минутах, секундах");
+            do
+            {
+                Console.Write("Градусы: ");
+                beGradus = int.TryParse(Console.ReadLine(), out gradus);
+                if (!beGradus)
+                {
+                    Console.WriteLine("Ошибка! Неверный формат данных. Попробуйте ещё раз.");
+                }
+            } while (!beGradus);
+            do
+            {
+                Console.Write("Минуты: ");
+                beMin = int.TryParse(Console.ReadLine(), out min);
+                if (!beMin)
+                {
+                    Console.WriteLine("Ошибка! Неверный формат данных. Попробуйте ещё раз.");
+                }
+            } while (!beMin);
+            do
+            {
+                Console.Write("Секунды: ");
+                beSec = int.TryParse(Console.ReadLine(), out sec);
+                if (!beSec)
+                {
+                    Console.WriteLine("Ошибка! Неверный формат данных. Попробуйте ещё раз.");
+                }
+            } while (!beSec);
+
+            Angle angle = new Angle(gradus, min, sec);
+            Console.WriteLine();
+            Console.WriteLine("Перевод градусов в радианы:");
+            Console.WriteLine("{0}°{1}'{2}\" = {3} рад.", gradus, min, sec, Math.Round(angle.ToRadians(),4, MidpointRounding.AwayFromZero));
+            Console.ReadKey();
+        }
+    }
+}
